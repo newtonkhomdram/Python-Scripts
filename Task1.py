199e1474aadb1d79d0571206b720d17fc7ca0b00
Newton Khomdram <newtonkhomdram7@gmail.com>
2026-01-17 23:51:50 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,35 @@
+## Task 1: Perform Basic Mathematical Operations.
+## 1.1 Takes two numbers as input from the user.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the secound number "))
+
+## Performs the basic mathematical operations on these two numbers
+## 1.2 Addition, Subtraction, Multiplication, Division.
+## Addition
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the secound number: "))
+sum = a + b 
+print("Sum =",sum)
+
+## Subtraction
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the secound number: "))
+sub = a - b 
+print("Subtraction =",sub)
+
+## Multiplication 
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the secound number: "))
+mul = a * b
+print("Multiplication =",mul)
+
+## Division
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the secound number: "))
+div = a / b
+print("Division =",div)
---
+++ Task2.py
@@ -0,0 +1,19 @@
+## Task 2: Create a Personalized Greeting.
+## 2.1 Takes a user's first name and last name as input.
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+## 2.2 Concatenates the first name and last name into a full name
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+fullname = a + " " + b
+print("Your full name is",fullname)
+
+## 2.3 Prints a personalized greeting message using the full name.
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+fullname = a + " " + b
+print("Hello,",fullname,"Great to see you today!")
